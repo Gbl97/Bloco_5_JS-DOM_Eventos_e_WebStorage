90a198b8e0abd16d3044e8f6b709f93d875cd703
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-06 14:27:44 -0300

    Exercicio 3 - Preciso rever o codigo

--- dia_3/exercise.js
+++ dia_3/exercise.js
@@ -1,29 +1,29 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+
     for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
     };
-  };
+};
 
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
 
-   // Escreva seu código abaixo.
-  
+// Escreva seu código abaixo.
 
-  function createDaysOfTheMonth() {
+
+function createDaysOfTheMonth() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     const GetDayList = document.querySelector('#days');
 
     for (let index = 0; index < dezDaysList.length; index += 1) {
         const dayArray = dezDaysList[index];
         const dayListItem2 = document.createElement('li');
-        
+
         if (dayArray === 4 | dayArray === 11 | dayArray === 18) {
             dayListItem2.className = 'day holiday';
             dayListItem2.innerHTML = dayArray;
@@ -40,13 +40,13 @@ function createDaysOfTheWeek() {
             dayListItem2.className = 'day';
             dayListItem2.innerHTML = dayArray;
             GetDayList.appendChild(dayListItem2);
-        } 
+        }
     };
-  };
+};
 
-  createDaysOfTheMonth();
+createDaysOfTheMonth();
 
-  function createButtonsContainer(buttonName) {
+function createButtonsContainer(buttonName) {
     let getFatherBtnContainer = document.querySelector('.buttons-container');
     let childBtnContainer = document.createElement('button');
     let childBtnContainerID = 'btn-holiday';
@@ -57,7 +57,31 @@ function createDaysOfTheWeek() {
     getFatherBtnContainer.appendChild(childBtnContainer);
 
     // 2º forma para adicionar um nó texto entre tags: childBtnContainer.textContent
-  };
-  
-  createButtonsContainer('Feriado');
- 
+};
+
+createButtonsContainer('Feriado');
+
+function executeChangeBackground() {
+    let executaEvento;
+    let getBtnHoliday = document.querySelector('#btn-holiday');
+
+    executaEvento = getBtnHoliday.addEventListener('click',eventChangeBackground);
+};
+
+function eventChangeBackground() {
+    let getHoliday = document.querySelector('.holiday');
+    let backgroundColor = 'rgb(238,238,238)';
+    let setColor = 'rgb(255,255,255)';
+
+    for (let index = 0; index < getHoliday.length; index += 1) {
+        if (getHoliday[index].style.backgroundColor === setColor) {
+            getHoliday[index].style.backgroundColor = backgroundColor;
+        } else {
+            getHoliday[index].style.backgroundColor = setColor;
+        }
+        
+    }
+};
+
+executeChangeBackground();
+
