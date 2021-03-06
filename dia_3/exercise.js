9f01b9e36b916c6c752491cd17eeafe474a7bf5d
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-05 22:11:36 -0300

    Exercicio 1 - Criando a semanas e os dias do calendario dinamicamente feito

--- dia_3/exercise.js
+++ dia_3/exercise.js
@@ -10,7 +10,40 @@ function createDaysOfTheWeek() {
       weekDaysList.appendChild(dayListItem);
     };
   };
-  
+
   createDaysOfTheWeek();
+
+   // Escreva seu código abaixo.
   
-  // Escreva seu código abaixo.
+
+  function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const GetDayList = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const dayArray = dezDaysList[index];
+        const dayListItem2 = document.createElement('li');
+        
+        if (dayArray === 4 | dayArray === 11 | dayArray === 18) {
+            dayListItem2.className = 'day holiday';
+            dayListItem2.innerHTML = dayArray;
+            GetDayList.appendChild(dayListItem2);
+        } else if (dayArray === 24 | dayArray === 31) {
+            dayListItem2.className = 'day holiday';
+            dayListItem2.innerHTML = dayArray;
+            GetDayList.appendChild(dayListItem2);
+        } else if (dayArray === 25) {
+            dayListItem2.className = 'day friday holiday';
+            dayListItem2.innerHTML = dayArray;
+            GetDayList.appendChild(dayListItem2);
+        } else {
+            dayListItem2.className = 'day';
+            dayListItem2.innerHTML = dayArray;
+            GetDayList.appendChild(dayListItem2);
+        } 
+    };
+  };
+
+  createDaysOfTheMonth();
+  
+ 
